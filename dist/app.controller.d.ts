7cce26ec455f55a180d18ef73fe42394f253d5fc
Gakuto Watanabe <g_watanabe@itandi.co.jp>
2023-05-27 22:52:40 +0900

    create backend directory

---
+++ dist/app.controller.d.ts
@@ -0,0 +1,22 @@
+import { UserService } from './user.service';
+import { PostService } from './post.service';
+import { User as UserModel, Post as PostModel } from '@prisma/client';
+export declare class AppController {
+    private readonly userService;
+    private readonly postService;
+    constructor(userService: UserService, postService: PostService);
+    getPostById(id: string): Promise<PostModel>;
+    getPublishedPosts(): Promise<PostModel[]>;
+    getFilteredPosts(searchString: string): Promise<PostModel[]>;
+    createDraft(postData: {
+        title: string;
+        content?: string;
+        authorEmail: string;
+    }): Promise<PostModel>;
+    signupUser(userData: {
+        name?: string;
+        email: string;
+    }): Promise<UserModel>;
+    publishPost(id: string): Promise<PostModel>;
+    deletePost(id: string): Promise<PostModel>;
+}
